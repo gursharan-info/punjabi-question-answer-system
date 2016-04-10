0e236f7a8ce6a040ac748cb8d75e75788fee4815
gursharan <gursharan-info>
2016-04-10 11:40:33 +0530

    Deploying on DigitalOcean

--- qa/urls.py
+++ qa/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'qa'
 urlpatterns = [
-    url(r'^qa$', views.index, name='index'),
-    url(r'^qa/comprehension$', views.comprehensions, name='comprehensions'),
-    url(r'^qa/comprehension/(?P<comprehension_id>[0-9]+)/$', views.compdetail, name='compdetail'),
-    url(r'^qa/question/(?P<question_id>[0-9]+)/$', views.questiondetail, name='questiondetail'),
-    url(r'^qa/dictionary/$', views.dictionary, name='dictionary'),
-    url(r'^qa/patterns/$', views.patterns, name='patterns'),
-    url(r'^qa/toppatterns/$', views.top_patterns, name='toppatterns'),
+    url(r'^$', views.index, name='index'),
+    url(r'^comprehension/$', views.comprehensions, name='comprehensions'),
+    url(r'^comprehension/(?P<comprehension_id>[0-9]+)/$', views.compdetail, name='compdetail'),
+    url(r'^question/(?P<question_id>[0-9]+)/$', views.questiondetail, name='questiondetail'),
+    url(r'^dictionary/$', views.dictionary, name='dictionary'),
+    url(r'^patterns/$', views.patterns, name='patterns'),
+    url(r'^toppatterns/$', views.top_patterns, name='toppatterns'),
 ]
